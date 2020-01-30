03ff1db86c20c3b7c8aa2c50c6cc4566d22e6e49
Jeong Won An <jw3an@uwaterloo.ca>
2020-01-30 18:42:13 -0500

    Changed the local db location

--- lib/shared/config.dart
+++ lib/shared/config.dart
@@ -2,4 +2,4 @@ import 'package:fund_tracker/shared/constants.dart';
 
 const DatabaseType DATABASE_TYPE = DatabaseType.Local;
 
-const String LOCAL_DATABASE_FILENAME = 'Test8';
+const String LOCAL_DATABASE_FILENAME = 'release-v1.0';
