6a5715a4d476c74c42316015741f8c740e0f8c29
Jeong Won An <jw3an@uwaterloo.ca>
2020-01-29 21:29:31 -0500

    Set up stats page

--- lib/pages/statistics/statistics.dart
+++ lib/pages/statistics/statistics.dart
@@ -36,77 +36,80 @@ class _StatisticsState extends State<Statistics> {
       }
     }
 
-    return Container(
+    Widget _transactionsSelection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            padding: EdgeInsets.all(15.0),
+            color: _showAllTimeStats
+                ? Theme.of(context).primaryColor
+                : Colors.grey[100],
+            child: Text(
+              'All-Time',
+              style: TextStyle(
+                  fontWeight:
+                      _showAllTimeStats ? FontWeight.bold : FontWeight.normal,
+                  color: _showAllTimeStats ? Colors.white : Colors.black),
+            ),
+            onPressed: () => setState(() {
+              _showAllTimeStats = true;
+              _showPreferredStats = false;
+              _showPeriodStats = false;
+            }),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            padding: EdgeInsets.all(15.0),
+            color: _showPreferredStats
+                ? Theme.of(context).primaryColor
+                : Colors.grey[100],
+            child: Text(
+              _visiblePrefs,
+              style: TextStyle(
+                  fontWeight:
+                      _showPreferredStats ? FontWeight.bold : FontWeight.normal,
+                  color: _showPreferredStats ? Colors.white : Colors.black),
+            ),
+            onPressed: () => setState(() {
+              _showPreferredStats = true;
+              _showAllTimeStats = false;
+              _showPeriodStats = false;
+            }),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            padding: EdgeInsets.all(15.0),
+            color: _showPeriodStats
+                ? Theme.of(context).primaryColor
+                : Colors.grey[100],
+            child: Text(
+              'Period',
+              style: TextStyle(
+                  fontWeight:
+                      _showPeriodStats ? FontWeight.bold : FontWeight.normal,
+                  color: _showPeriodStats ? Colors.white : Colors.black),
+            ),
+            onPressed: () => setState(() {
+              _showPeriodStats = true;
+              _showAllTimeStats = false;
+              _showPreferredStats = false;
+            }),
+          ),
+        ),
+      ],
+    );
+
+    return ListView(
       padding: EdgeInsets.symmetric(
         vertical: 20.0,
         horizontal: 10.0,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              padding: EdgeInsets.all(15.0),
-              color: _showAllTimeStats
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey[100],
-              child: Text(
-                'All-Time',
-                style: TextStyle(
-                    fontWeight:
-                        _showAllTimeStats ? FontWeight.bold : FontWeight.normal,
-                    color: _showAllTimeStats ? Colors.white : Colors.black),
-              ),
-              onPressed: () => setState(() {
-                _showAllTimeStats = true;
-                _showPreferredStats = false;
-                _showPeriodStats = false;
-              }),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              padding: EdgeInsets.all(15.0),
-              color: _showPreferredStats
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey[100],
-              child: Text(
-                _visiblePrefs,
-                style: TextStyle(
-                    fontWeight: _showPreferredStats
-                        ? FontWeight.bold
-                        : FontWeight.normal,
-                    color: _showPreferredStats ? Colors.white : Colors.black),
-              ),
-              onPressed: () => setState(() {
-                _showPreferredStats = true;
-                _showAllTimeStats = false;
-                _showPeriodStats = false;
-              }),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              padding: EdgeInsets.all(15.0),
-              color: _showPeriodStats
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey[100],
-              child: Text(
-                'Period',
-                style: TextStyle(
-                    fontWeight:
-                        _showPeriodStats ? FontWeight.bold : FontWeight.normal,
-                    color: _showPeriodStats ? Colors.white : Colors.black),
-              ),
-              onPressed: () => setState(() {
-                _showPeriodStats = true;
-                _showAllTimeStats = false;
-                _showPreferredStats = false;
-              }),
-            ),
-          ),
-        ],
-      ),
+      children: <Widget>[
+        _transactionsSelection,
+      ],
     );
   }
 }
